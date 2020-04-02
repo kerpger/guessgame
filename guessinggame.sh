b66cccc413c4b3991c835716a9c592cce33ef3e8
kerpger <ltkp@hotmail.com>
2020-04-02 15:44:52 +0200

    Initialize guessingame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# guessinggame.sh
+nrfiles=$(ls $PWD | wc -l)
+msg=("Incorrect, Guess higher number" "Incorrect, Guess lower number" "Correct, Congratulation!")
+sol=""
+
+function check {
+if [[ $answ -ne $nrfiles ]]
+then
+	[[ $answ -lt $nrfiles ]] && sol=${msg[0]} || sol=${msg[1]}
+else
+	sol=${msg[2]}
+fi
+}
+while [[ $answ -ne $nrfiles ]]
+do
+	echo -n "How many files are in the current directory ($PWD) ? "
+	read answ
+	check
+	echo $sol
+done
